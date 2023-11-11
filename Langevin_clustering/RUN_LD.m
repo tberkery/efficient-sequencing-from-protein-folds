a3881855dca74d9375b406b3bd83f3cfe401cfe3
20richardh <3dblackhorse@gmail.com>
2023-11-11 03:40:22 -0500

    equilibration steps

--- Langevin_clustering/RUN_LD.m
+++ Langevin_clustering/RUN_LD.m
@@ -4,8 +4,9 @@ clc; clear;
 
 %% 1) PUT IN INITIAL COORDINATES HERE
 %init_pos = [0,0,1; 0,0,2; 0,0,3; 0,0,4; 0,0,5];
-init_pos = [0,0,1; 0,0,2; 0,0,3; 0,0,4; 0,0,5; 0,0,6; 0,0,7; 0,0,8; 0,0,9; 0,0,10; 0,0,11; 0,0,12; 0,0,13; 0,0,14];
+seed_pos = [0,0,1; 0,0,2; 0,0,3; 0,0,4; 0,0,5; 0,0,6; 0,0,7; 0,0,8; 0,0,9; 0,0,10; 0,0,11; 0,0,12; 0,0,13; 0,0,14];
 num_clusters = 6; % expected number of clusters for the protein
+steps_equilibrate = 10^4;
 steps_msm = 10^4;
 steps_sim = steps_msm*10^2;
 
@@ -14,15 +15,21 @@ steps_sim = steps_msm*10^2;
 %is_hydrophil = [true;false;false;true;true];
 is_hydrophil = [false;false;false;true;true; true;false;false; true; true; true; false; false; false];
 
-%% 3) LD, MSM, AND FIND CLUSTER MEANS HERE
+%% 3 EQUILIBRATE HP CHAIN WITH LD
+[coords_equilibration, times_equilibration, potentials_equilibration, kinetics_equilibration, temperatures_equilibration] = ...
+    LD_to_coords(seed_pos, is_hydrophil, 1, 0.003, steps_msm);
+
+%% 4) RUN EQUILIBRATED LD FOR THE MSM, AND FIND CLUSTER MEANS
+init_pos = coords_equilibration(:,:,1);
 [coords_v_time, times, potentials, kinetics, temperatures] = LD_to_coords(init_pos, is_hydrophil, 1, 0.003, steps_msm);
 [clust_means, transition_matrix, markovian_check] = msm(coords_v_time, num_clusters); % ones(1,7); % placeholder
 
-%% 4) RUN LD SIMULATION HERE
+%% 5) RUN LD SIMULATION HERE
 % running LD with T=2, dt=0.003, steps = 10,000 (need to change)
-[clusters, times, potentials, kinetics, temperatures, equilibrium_pos] = LD_to_clusters(init_pos, is_hydrophil, 1, 0.003, steps_sim, clust_means);
+simStart_pos = coords_v_time(:,:,1);
+[clusters, times, potentials, kinetics, temperatures, equilibrium_pos] = LD_to_clusters(simStart_pos, is_hydrophil, 1, 0.003, steps_sim, clust_means);
 
-%% 5) Write clusters to a sequence file
+%% 6) Write clusters to a sequence file
 %clusters
 report_clusters(clusters, "sequence.csv", NaN);
 
