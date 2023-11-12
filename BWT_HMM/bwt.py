355acfb8a2f312e5bb6696ba8d211f2f30f14075
nyoh17 <yoh17@jh.edu>
2023-11-12 11:50:13 -0500

    initial BWT-HMM code using hmmlearn

---
+++ BWT_HMM/bwt.py
@@ -0,0 +1,37 @@
+from hmmlearn import hmm
+import numpy as np
+
+def burrows_wheeler_transform(s):
+    """ Apply Burrows-Wheeler Transform to a given string. """
+    # Append a unique symbol to the end of the string to mark its end
+    s = s + '$'
+    # Create a table of rotations of the string
+    table = sorted(s[i:] + s[:i] for i in range(len(s)))
+    # Extract the last column
+    last_column = ''.join(row[-1] for row in table)
+    return last_column
+
+# Example protein conformation data as a string of numbers from 1 to 6
+example_data = "123456123456"
+# Apply BWT to the example data
+bwt_result = burrows_wheeler_transform(example_data)
+bwt_result
+
+# Since we're dealing with discrete data (numbers 1 to 6), we use a Multinomial HMM
+# Number of states in the HMM needs to be defined. This requires domain knowledge or experimentation.
+n_states = 4  # Example value
+
+# Initialize the HMM
+model = hmm.MultinomialHMM(n_components=n_states)
+
+# The data needs to be in a specific format for hmmlearn, usually as a 2D numpy array
+# Here, each number is considered a separate observation
+# For example, '66$1122334455' becomes [[6], [6], [1], [1], [2], [2], [3], [3], [4], [4], [5], [5]]
+observed_data = np.array([[int(x)] for x in bwt_result if x.isdigit()])
+
+# Training the model (this requires actual data and might need to adjust the parameters)
+# For demonstration, I am just using the observed_data for training, but in practice,
+# you'd train this on a larger, representative dataset
+model.fit(observed_data)
+
+# After training, this model can be used to predict states or evaluate new observations
