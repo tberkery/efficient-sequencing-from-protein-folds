63b3563de11381d3dd9cd0b28ce3b2121b330b14
tberkery <78045727+tberkery@users.noreply.github.com>
2023-11-13 00:14:58 -0500

    not yet a functional solution but code is now running

--- BWT_HMM/bwt-hmm.py
+++ BWT_HMM/bwt-hmm.py
@@ -14,7 +14,7 @@ def burrows_wheeler_transform(s):
 # Example protein conformation data as a string of numbers from 1 to 6
 example_data = "123456123456"
 # Apply BWT to the example data
-bwt_result = np.array(burrows_wheeler_transform(example_data))
+bwt_result = burrows_wheeler_transform(example_data)
 print(bwt_result)
 print(type(bwt_result))
 
@@ -24,14 +24,14 @@ n_states = 4  # Example value
 
 # Initialize the HMM
 model = hmm.MultinomialHMM(n_components=n_states)
-model.startprob_ = np.array([1.0 / n_states] for i in range(n_states)) # initially assume equal probability of each state
-model.transmat_ = np.array([[1.0 / n_states**2] for i in range(n_states)] for j in range(n_states))
-
+start_probs = np.full(n_states, 1.0 / n_states)  # initially assume equal probability of each state
+transmat_probs = np.full((n_states, n_states), 1.0 / n_states**2)
+model.set_params(startprob_prior = start_probs, transmat_prior = transmat_probs)
 
 # The data needs to be in a specific format for hmmlearn, usually as a 2D numpy array
 # Here, each number is considered a separate observation
 # For example, '66$1122334455' becomes [[6], [6], [1], [1], [2], [2], [3], [3], [4], [4], [5], [5]]
-observed_data = np.array([[int(x)] for x in bwt_result if x.isdigit()])
+observed_data = np.array([[int(x)] for x in bwt_result if x.isdigit()]) # Note that $ get filtered out
 
 # Training the model (this requires actual data and might need to adjust the parameters)
 # For demonstration, I am just using the observed_data for training, but in practice,
