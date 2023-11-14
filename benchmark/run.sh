88ad3f8ef1ac9ecc2919598c30bdee258eea7778
tberkery <tberker2@jhu.edu>
2023-11-14 10:04:53 -0500

    believe last issue is a memory error problem on my laptop (will try again later today on desktop). just designed bash script to run profile.py 50 times. idea is here is that  by running 50 separate times with 1 iteration per data structure/sample instead of 1 time wit 50 iterations per data structure/sample I will get some autonomous garbage collection in effect that doesn't cause memory to increase for future iterations.

---
+++ benchmark/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+command_to_run="python .\benchmark\profile.py > .\space_complexity_info.txt"
+
+# Run the command 50 times
+for ((i=1; i<=50; i++)); do
+    echo "Running command iteration $i"
+    $command_to_run
+done
