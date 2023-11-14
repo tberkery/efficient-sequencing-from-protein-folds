c86d82a5f30b0074e83dc70ab70aecaff4fc1e8a
tberkery <78045727+tberkery@users.noreply.github.com>
2023-11-14 15:28:26 -0500

    script is a hair delicate but extracting memory usage accounting for variable number of lines of output depending on data structure.

--- benchmark/parse_memory_usage_report.py
+++ benchmark/parse_memory_usage_report.py
@@ -11,20 +11,20 @@ with open(mem_usage_report_file, "r") as fp:
         if use_next:
             if local_count == 2:
                 line_split = line.split("    ") # this is not a tab, but rather a copy of a specific number of spaces
-                print(line_split)
+                #print(line_split)
                 mem_usage = line_split[2].split()
                 memory_used = mem_usage[0]
                 
                 try:
-                    print("found valid fourth line")
+                    #print("found valid fourth line")
                     fourth_local_line = fp.next()
                     line_split = fourth_local_line.split("    ") # this is not a tab, but rather a copy of a specific number of spaces
                     mem_usage = line_split[2].split()
                     memory_used = mem_usage[0]
                 except:
-                    print("did not find valid fourth line")
+                    #print("did not find valid fourth line")
                     useless = True # nothing to do here other than break out of the try
-                print(memory_used)
+                #print(memory_used)
                 df.loc[len(df.index)] = [data_structure_names[i % 4], sample_names[i % 4], memory_used]
                 i += 1
                 use_next = False
@@ -32,6 +32,7 @@ with open(mem_usage_report_file, "r") as fp:
                 local_count += 1
  
         if line[0:1] == "=":
-            print("detected =")
             use_next = True
             local_count = 0
+
+df.to_csv("memory_usage.csv", index = False)
