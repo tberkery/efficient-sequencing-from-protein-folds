09decbc4af0e5500beaebc66266a878cfa85c0a4
tberkery <78045727+tberkery@users.noreply.github.com>
2023-11-14 15:45:06 -0500

    revamped a few visuals to have better intervals and labels

--- benchmark/visualize_runtime.R
+++ benchmark/visualize_runtime.R
@@ -2,30 +2,32 @@ library(tidyverse)
 library(ggplot2)
 library(gridExtra)
 
-df_tc = read_csv("benchmarking_results.csv") %>%
-  filter(iteration <= 9)
+df_tc = read_csv("benchmarking_results.csv")
 # assuming protein-fold-pipeline is your working directory
 
 plots = vector(mode = "list", length = 4)
 data_structures = as.vector(unique(df_tc$data_structure))
 sample_types = as.vector(unique(df_tc$sample))
 colors = c("blue", "blue", "blue", "blue")
-binwidths = c(15, 0.01, 0.1, 0.005)
+binwidths = c(1, 0.01, 0.2, 0.005)
 counter = 1
-
+sample_titles = c("Full", "Comp", "Full", "Comp")
 for (ds in data_structures) {
   for (s in sample_types) {
     df_tc_sub = df_tc %>%
       filter(data_structure == ds, sample == s)
     p = ggplot(df_tc_sub, aes(x = runtime)) +
-      geom_histogram(binwidth = binwidths[counter])
+      geom_histogram(binwidth = binwidths[counter]) +
+      labs(title = paste0(ds, ", ", sample_titles[counter]),
+           x = "Runtime (seconds)",
+           y = "Density")
     plots[[counter]] = p
     counter = counter + 1
   }
 }
 
 grid.arrange(plots[[1]], plots[[2]], plots[[3]], plots[[4]], ncol = 2)
-ggsave("time_complexity_histograms.png")
+# ggsave("time_complexity_histograms.png") # cannot save a grid arrange object... this line just saves last plot in grid
 
 df_tc_100k = df_tc %>%
   filter(sample == "hypothetical_protein_test")
--- benchmark/visualize_space.R
+++ benchmark/visualize_space.R
@@ -11,20 +11,24 @@ sample_types = as.vector(unique(df_tc$sample))
 colors = c("blue", "blue", "blue", "blue")
 binwidths = c(0.5, 0.1, 0.5, 0.1)
 counter = 1
+sample_titles = c("Full", "Comp", "Full", "Comp")
 
 for (ds in data_structures) {
   for (s in sample_types) {
     df_tc_sub = df_tc %>%
       filter(data_structure == ds, sample == s)
     p = ggplot(df_tc_sub, aes(x = memory_usage)) +
-      geom_histogram(binwidth = binwidths[counter])
+      geom_histogram(binwidth = binwidths[counter]) +
+      labs(title = paste0(ds, ", ", sample_titles[counter]),
+           x = "Memory Used (MiB)",
+           y = "Density")
     plots[[counter]] = p
     counter = counter + 1
   }
 }
 
 grid.arrange(plots[[1]], plots[[2]], plots[[3]], plots[[4]], ncol = 2)
-ggsave("space_complexity_histograms.png", width = 12, height = 8, units = "in")
+#ggsave("space_complexity_histograms.png", width = 12, height = 8, units = "in") # cannot save a grid arrange object... this line just saves last plot in grid
 
 df_tc_100k = df_tc %>%
   filter(sample == "hypothetical_protein_test")
@@ -33,12 +37,12 @@ df_tc_comp = df_tc %>%
 p1 = ggplot(df_tc_100k, aes(x = memory_usage, fill = data_structure)) +
   geom_density() +
   labs(title = "Space Complexity for Example Protein",
-       x = "Space Complexity",
+       x = "Memory Used (MiB)",
        y = "Density")
 p2 = ggplot(df_tc_comp, aes(x = memory_usage, fill = data_structure)) +
   geom_density() +
   labs(title = "Space Complexity for Compressed Protein",
-       x = "Space Complexity",
+       x = "Memory Used (MiB)",
        y = "Density")
 
 ggsave("space_density_test_protein.png", p1, width = 12, height = 8, units = "in")
