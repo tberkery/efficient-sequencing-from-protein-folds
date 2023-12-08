4dbf8b2f3557a851594f4235c5b7c934162ff20c
tberkery <78045727+tberkery@users.noreply.github.com>
2023-12-08 14:24:51 -0500

    benchmarking updated for bwt-hmm merging and updated version of MLSE viterbi and is working (at least on a quick test case of 1 iteration)

--- benchmark/parse_memory_usage_report.py
+++ benchmark/parse_memory_usage_report.py
@@ -3,29 +3,25 @@ import pandas as pd
 mem_usage_report_file = sys.argv[1]
 use_next = False
 df = pd.DataFrame(columns = ["data_structure", "sample", "memory_usage"])
-data_structure_names = ["BWT-HMM", "BWT-HMM", "MLSE-Viterbi", "MLSE-Viterbi"]
-sample_names = ["hypothetical_protein_test", "compressed_hypothetical_protein_test", "hypothetical_protein_test", "compressed_hypothetical_protein_test"]
+data_structure_names = ["BWT-HMM", "BWT-HMM", "MLSE-Viterbi", "MLSE-Viterbi", "BWT-HMM-Merging", "BWT-HMM-Merging"]
+sample_names = ["hypothetical_protein_test", "compressed_hypothetical_protein_test", "hypothetical_protein_test", "compressed_hypothetical_protein_test", "hypothetical_protein_test", "compressed_hypothetical_protein_test"]
 i = 0
 with open(mem_usage_report_file, "r") as fp:
     for line in fp.readlines():
         if use_next:
             if local_count == 2:
                 line_split = line.split("    ") # this is not a tab, but rather a copy of a specific number of spaces
-                #print(line_split)
                 mem_usage = line_split[2].split()
                 memory_used = mem_usage[0]
                 
                 try:
-                    #print("found valid fourth line")
                     fourth_local_line = fp.next()
                     line_split = fourth_local_line.split("    ") # this is not a tab, but rather a copy of a specific number of spaces
                     mem_usage = line_split[2].split()
                     memory_used = mem_usage[0]
                 except:
-                    #print("did not find valid fourth line")
                     useless = True # nothing to do here other than break out of the try
-                #print(memory_used)
-                df.loc[len(df.index)] = [data_structure_names[i % 4], sample_names[i % 4], memory_used]
+                df.loc[len(df.index)] = [data_structure_names[i % len(data_structure_names)], sample_names[i % len(data_structure_names)], memory_used]
                 i += 1
                 use_next = False
             else:
@@ -35,4 +31,4 @@ with open(mem_usage_report_file, "r") as fp:
             use_next = True
             local_count = 0
 
-df.to_csv("memory_usage.csv", index = False)
+df.to_csv("memory_usage_updated.csv", index = False)
