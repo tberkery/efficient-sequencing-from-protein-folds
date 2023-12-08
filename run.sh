55ab221ad0b979e6b70b9194ffb3102de57220ca
tberkery <78045727+tberkery@users.noreply.github.com>
2023-12-08 18:16:11 -0500

    experimenting with larger experiment (more iterations) on whole sequence (if possible given computing constraints)

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 command_to_run="python ./benchmark/profile.py space_complexity_info_updated.txt benchmarking_results_updated.csv > space_complexity_info_updated.txt"
 
 # Run the command 50 times
-for ((i=1; i<=25; i++)); do
+for ((i=1; i<=100; i++)); do
     echo "Running command iteration $i"
     $command_to_run
 done
