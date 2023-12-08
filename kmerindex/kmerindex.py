b8e8f5561909096c5c6dbbd68bfad0221b4f81d9
Lily Chen <xchen183@jhu.edu>
2023-12-07 23:45:05 -0500

    Modified kmerindex to survey a range of kmer lengths

--- kmerindex/kmerindex.py
+++ kmerindex/kmerindex.py
@@ -1,37 +1,44 @@
 import sys
 
-inputName = "../generate_sequence/simulation.txt"
-k = 10
+inputName = "../generate_sequence/simulation.txt" #"compressed_MDseq.txt"
+#k = 7
 outputName = "output.txt"
 
-# Read the sequence into a list then convert to string
-with open(inputName, 'r') as ifile:
-    lines = ifile.readlines()
 
-sequence = ''
-for i in range(len(lines)):
-    sequence = sequence + lines[i].strip()
+def kmerindex(inputName, kmin, kmax):
+    # Read the sequence into a list then convert to string
+    with open(inputName, 'r') as ifile:
+        lines = ifile.readlines()
 
-kmerIndex = {}
-for i in range(len(sequence) - k + 1):
-    kmer = sequence[i:i+k]
-    if kmer not in kmerIndex:
-        kmerIndex[kmer] = []
-    kmerIndex[kmer].append(i)
+    # Process sequence
+    sequence = ''
+    for i in range(len(lines)):
+        sequence = sequence + lines[i].strip()
 
-# Write the length of the kmerIndex into a file
-with open(outputName, 'w') as ofile:
-    ofile.write(str(len(kmerIndex)))
-
-
-# Find most common kmer
-commonkmers = []
-for kmer in kmerIndex:
-    number = len(kmerIndex[kmer])
-    commonkmers.append((number, kmer))
     
-commonkmers.sort(reverse=True)
-for i in range(5):
-    print(commonkmers[i]) 
+    # Records all kmers with their occurances in the form (occurance * length, kmer)
+    commonkmers = []
 
-#print(commonKmer, number)
+    # Make kmer of different lengths and find best matches
+    for k in range(kmin, kmax):
+
+        # Build kmer index
+        kmerIndex = {}
+        for i in range(len(sequence) - k + 1):
+            kmer = sequence[i:i+k]
+            if kmer not in kmerIndex:
+                kmerIndex[kmer] = []
+            kmerIndex[kmer].append(i)
+
+        # Append all kmers in the form (occurance * length, kmer)
+        for kmer in kmerIndex:
+            number = len(kmerIndex[kmer])
+            commonkmers.append((len(kmer) * number, kmer))
+    
+    # Print out the 5 best matches
+    commonkmers.sort(reverse=True)
+    return commonkmers[:10]
+
+
+bestkmers = kmerindex("../generate_sequence/simulation.txt", kmin=5, kmax=15)
+print(bestkmers)
